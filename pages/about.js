788d67321a525ee160a5e56829204f42d66f5343
Peter Darche <pdarche@gmail.com>
2018-06-03 15:14:33 -0400

    Move researcher info to config

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Meta from "../components/Meta";
 import Nav from "../components/Nav";
 import Survey from "../components/Survey";
 import { styles } from "../styles/about";
+import { LEAD_RESEARCHER, LEAD_RESEARCHER_EMAIL } from "../config";
 
 export default class SurveyPage extends React.Component {
   constructor(props) {
@@ -23,15 +24,14 @@ export default class SurveyPage extends React.Component {
               <h2>About the Project</h2>
               <p>
                 This application part of research project on social value
-                orientation. The goal is to better understand what explains the
-                variation orientations. Peter Darche is the primary 
+                orientation. { LEAD_RESEARCHER } is the primary
                 researcher. Please reach out to him with any questions or
-                feedback <a href="mailto:pdarche@gmail.com">here</a>.
+                feedback <a href={ LEAD_RESEARCHER_EMAIL }>here</a>.
               </p>
               <h2>Data Privacy</h2>
               <p>
-                The data associated with this survey is anonymous and will not
-                be shared with anyone. Only the research team will see anything
+                The data associated with this survey is anonymous.
+                Only the research team will see information
                 associated with application. Most data is stored on your
                 computer and only communicated once over a secure connection.
               </p>
